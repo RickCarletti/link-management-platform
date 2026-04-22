d72a360fcd07840b5c759605a66baa8091ae8a35
Ricardo Carletti <ricardocarletti@id.uff.br>
2026-04-22 10:44:33 -0300

    chore: standardize quotes and formatting in configuration and test files

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -4,18 +4,18 @@ import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 
 // Carrega as variáveis do arquivo .env para o process.env
-dotenv.config({quiet: true});
+dotenv.config({ quiet: true });
 
 // Verifica se a variável existe para evitar erros silenciosos
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL não encontrada no arquivo .env');
 }
 
-const pool = new Pool({ 
-  connectionString: process.env.DATABASE_URL 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
 const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient( { adapter } );
+const prisma = new PrismaClient({ adapter });
 
 export { prisma };
